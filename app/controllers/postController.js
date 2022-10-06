1e27988a8d619d7174a1cd410bbb683c01232cea
Abdul Rahman <abdul.rahman@talentica.com>
2022-10-06 21:59:26 +0530

    added get post,posts n delete apis

--- app/controllers/postController.js
+++ app/controllers/postController.js
@@ -1,8 +1,9 @@
 const Posts = require('../models/posts');
 const logger = require('../helpers/logger');
 const { CREATE_POST } = require('../consts/events');
-const { uploadFile } = require('../helpers/aws/uploader');
 const { push } = require('../events/sqs');
+const { uploadFile, deleteFile } = require('../helpers/aws/uploader');
+const dbHelper = require('../helpers/dbHelper/posts');
 const { S3_BUCKET_NAME: bucketName } = require('config');
 
 
@@ -20,8 +21,31 @@ const createPost = async (data, file) => {
         msg: 'post created',
         data: createdPost
     };
-}
+};
+
+const getPost = async (postId) => {
+    return await dbHelper.fetchPost(postId);
+};
+
+const getPosts = async (userId) => {
+    return await dbHelper.fetchPosts(userId);
+};
+
+const deletePost = async (postId) => {
+    logger.info('deletePost Controller...');
+    const postDetails = await dbHelper.fetchPost(postId)
+    const deletedFile = await deleteFile(postDetails.objectKey);
+    logger.info('post deleted', deletedFile);
+    const deletedPost = await dbHelper.deletePost(postId)
+    return {
+        msg: 'post deleted',
+        data: deletedPost
+    };
+};
 
 module.exports = {
-    createPost
+    createPost,
+    getPost,
+    getPosts,
+    deletePost
 };
